a4be03e600042f26ca5a3683105a3fbb25cae3e1
Noman00133 <b00133433@mytudublin.ie>
2023-10-27 22:20:32 +0100

    xor_Properties.py

--- CryptoHack/xorProperties.py
+++ CryptoHack/xorProperties.py
@@ -1,22 +1,60 @@
-from pwn import xor	
-
-# Given values on the challenge question
-
-key1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
-key1_2 = "37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e"
-key2_3 = "c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1"
-flag_key123 = "04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf"
+def shift_rows(s): # shift the arrays to the left
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
 
 
-# Solving for individual key values
-
-key2 = xor(bytes.fromhex(key1_2), key1)
-key3 = xor(bytes.fromhex(key2_3), key2)
-
-key1_2_3 = xor(bytes.fromhex(key1_2), key3)
+def inv_shift_rows(s): # shifts the rows of the array to the right
+    s[1][1], s[2][1], s[3][1], s[0][1] = s[0][1], s[1][1], s[2][1], s[3][1]
+    s[2][2], s[3][2], s[0][2], s[1][2] = s[0][2], s[1][2], s[2][2], s[3][2]
+    s[3][3], s[0][3], s[1][3], s[2][3] = s[0][3], s[1][3], s[2][3], s[3][3]
 
 
-flag = xor(bytes.fromhex(flag_key123), key1_2_3)
+# learned from http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
 
 
-print(flag.decode())
+def mix_single_column(a):
+    # see Sec 4.1.2 in The Design of Rijndael
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
+    a[3] ^= t ^ xtime(a[3] ^ u)
+
+
+def mix_columns(s):
+    for i in range(4):
+        mix_single_column(s[i])
+
+
+def inv_mix_columns(s):
+    # see Sec 4.1.3 in The Design of Rijndael
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mix_columns(s)
+
+
+state = [
+    [108, 106, 71, 86],
+    [96, 62, 38, 72],
+    [42, 184, 92, 209],
+    [94, 79, 8, 54],
+]
+
+# Inverse mix_colums and inverse shiftrows to state.
+inv_mix_columns(state)
+inv_shift_rows(state)
+
+#print the result
+for a in range(4):
+    for b in range(4):
+        print(chr(state[a][b]), end="")
+
